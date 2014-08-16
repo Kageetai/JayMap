a3a425934a758eb248299cd49c2c3ac416d5018f
Kageetai <ma@kageetai.net>
2014-08-16 22:52:39 +0100

    ability to add a new shop to the database

--- client/app/shop/new/new.controller.js
+++ client/app/shop/new/new.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('jayMapApp')
-  .controller('NewCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('NewCtrl', function ($scope, $http) {
+      $scope.addShop = function() {
+        $scope.submittedSuccess = true;
+
+        if($scope.newShop === {}) {
+          return;
+        }
+        $http.post('/api/shops', {
+          name: $scope.newShop.name,
+          address: $scope.newShop.address
+        });
+        $scope.newShop = {};
+      };
   });
