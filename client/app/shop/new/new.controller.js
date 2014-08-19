db3910babb7c267e8668069a0190d06e8e47cdd5
Kageetai <ma@kageetai.net>
2014-08-19 13:05:06 +0100

    address fetched from google maps api and resolved to coordinates verfication of form

--- client/app/shop/new/new.controller.js
+++ client/app/shop/new/new.controller.js
@@ -2,18 +2,36 @@
 
 angular.module('jayMapApp')
   .controller('NewCtrl', function ($scope, $http) {
-      $scope.addShop = function() {
-        $scope.submittedSuccess = true;
+    $scope.addShop = function () {
+      $scope.submitted = true;
 
-        if($scope.newShop === {}) {
-          return;
-        }
-        $http.post('/api/shops', {
-          name: $scope.newShop.name,
-          address: $scope.newShop.address,
-          latitude: $scope.newShop.latitude,
-          longitude: $scope.newShop.longitude
+      if ($scope.newShop === {}) {
+        return;
+      }
+
+      if (form.$valid) {
+        $http({
+          method: 'get',
+          url: 'http://maps.googleapis.com/maps/api/geocode/json',
+          params: { address: $scope.newShop.address.replace(' ', '+') }
+        }).success(function (data, status) {
+          if (data.status === 'OK') {
+            $scope.newShop.latitude = data.results[0].geometry.location.lat;
+            $scope.newShop.longitude = data.results[0].geometry.location.lng;
+
+            $http.post('/api/shops', {
+              name: $scope.newShop.name,
+              address: $scope.newShop.address,
+              latitude: $scope.newShop.latitude,
+              longitude: $scope.newShop.longitude
+            }).success(function () {
+              $scope.newShop = {};
+              $scope.submittedSuccess = true;
+            });
+          } else if (data.status === 'ZERO_RESULTS') {
+            $scope.submittedNoLocation = true;
+          }
         });
-        $scope.newShop = {};
-      };
+      }
+    };
   });
