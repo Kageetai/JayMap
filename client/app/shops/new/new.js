7e258638a203aec84d046a13dd2e51faa1a34a20
Kageetai <ma@kageetai.net>
2014-11-09 23:55:36 +0000

    auth back for new shop

--- client/app/shops/new/new.js
+++ client/app/shops/new/new.js
@@ -5,8 +5,8 @@ angular.module('jayMapApp')
     $stateProvider
       .state('new', {
         url: '/shops/new',
-        templateUrl: 'app/shops/new/new.html',
-        controller: 'NewCtrl'//,
-        //authenticate: true
+        templateUrl: 'app/shops /new/new.html',
+        controller: 'NewCtrl',
+        authenticate: true
       });
   });
