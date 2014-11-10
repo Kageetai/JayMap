3d4755ddf04c70541409f4308e9f9aaf42953c36
Kageetai <ma@kageetai.net>
2014-11-10 14:55:40 +0000

    clustering of markers on main

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,45 +1,57 @@
 'use strict';
 
 angular.module('jayMapApp')
-    .controller('MainCtrl', function ($scope, $http) {
-      $scope.map = {
-        center: {
-          latitude: 52.5167,
-          longitude: 13.3833
-        },
-        zoom: 11,
-        draggable: true,
-        options: {
-          scrollwheel: true
-        }
-      };
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.map = {
+      center: {
+        latitude: 52.5167,
+        longitude: 13.3833
+      },
+      zoom: 11,
+      fit: true,
+      doCluster: true,
+      clusterOptions: {
+        title: 'Hi I am a Cluster!',
+        gridSize: 60,
+        ignoreHidden: true,
+        minimumClusterSize: 2,
+        imageExtension: 'png',
+        imagePath: 'assets/images/logo-green',
+        imageSizes: [32],
+        textColor: "white"
+      },
+      draggable: true,
+      options: {
+        scrollwheel: true
+      }
+    };
 
-      $scope.shops = [];
+    $scope.shops = [];
 
-      $http.get('/api/shops').success(function (shops) {
-        $scope.shops = shops;
+    $http.get('/api/shops').success(function (shops) {
+      $scope.shops = shops;
 
-        _.each($scope.shops, function (shop) {
-          shop.icon = 'assets/images/logo-green.png';
-          shop.closeClick = function () {
-            shop.showWindow = false;
-            $scope.$apply();
-          };
-          shop.onClicked = function () {
-            $scope.selShop = shop;
-          };
-        });
+      _.each($scope.shops, function (shop) {
+        shop.icon = 'assets/images/logo-green.png';
+        shop.closeClick = function () {
+          shop.showWindow = false;
+          $scope.$apply();
+        };
+        shop.onClicked = function () {
+          $scope.selShop = shop;
+        };
       });
-
-      $scope.addShop = function () {
-        if ($scope.newShop === '') {
-          return;
-        }
-        $http.post('/api/shops', { name: $scope.newShop });
-        $scope.newShop = '';
-      };
-
-      $scope.deleteShop = function (shop) {
-        $http.delete('/api/shops/' + shop._id);
-      };
     });
+
+    $scope.addShop = function () {
+      if ($scope.newShop === '') {
+        return;
+      }
+      $http.post('/api/shops', {name: $scope.newShop});
+      $scope.newShop = '';
+    };
+
+    $scope.deleteShop = function (shop) {
+      $http.delete('/api/shops/' + shop._id);
+    };
+  });
