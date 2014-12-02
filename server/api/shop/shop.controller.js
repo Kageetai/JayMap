eae3df63670ff7470a9f5095460bd430060801c9
Kageetai <ma@kageetai.net>
2014-12-02 20:32:46 +0000

    automatically update updatedAt field in shop when saving

--- server/api/shop/shop.controller.js
+++ server/api/shop/shop.controller.js
@@ -35,6 +35,7 @@ exports.update = function(req, res) {
     if (err) { return handleError(res, err); }
     if(!shop) { return res.send(404); }
     var updated = _.merge(shop, req.body);
+    shop.updatedAt = new Date();
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
       return res.json(200, shop);
